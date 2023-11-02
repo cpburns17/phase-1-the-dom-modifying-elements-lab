c722eff366c3be747fb425a9ddae73a21acc385f
Connor Burns <cpburns17@gmail.com>
2023-11-02 09:42:42 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const element = document.getElementById('main');
+const secondChild = element.querySelector('id')
+element.remove('#main');
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Connor Burns is the champion';
